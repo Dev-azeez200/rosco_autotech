42d1d9b4dad01eb688a4f493d74b4f4048ddfd33
Dev-azeez200 <azeezmustapha380@gmail.com>
2026-04-07 13:12:37 +0100

    update page path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import MainLayout from "./Layout/MainLayout";
+import MainLayout from "./Layout/mainlayout";
 import { Toaster } from "sonner";
 import About from "./Pages/About";
 import Service from "./Pages/Service";
